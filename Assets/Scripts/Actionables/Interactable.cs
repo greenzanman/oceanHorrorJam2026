6146eb05d6435b6af4d88dadbc58bec181be169a
greenzanman <112117557+greenzanman@users.noreply.github.com>
2026-02-19 10:19:44 -0500

    add missing import

--- Assets/Scripts/Actionables/Interactable.cs
+++ Assets/Scripts/Actionables/Interactable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using TMPro;
+using UnityEngine.InputSystem;
 
 public class Interactable : MonoBehaviour, Actionable
 {
